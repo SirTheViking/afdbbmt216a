71b2fc3743927a2c2dda79eea2f00cb52655df58
SirTheViking <alexhexan22@gmail.com>
2017-03-25 23:23:15 +0100

    Started work on the search command

--- src/main/java/GoogleSearch.java
+++ src/main/java/GoogleSearch.java
@@ -6,9 +6,7 @@ public class GoogleSearch extends Command {
     public void onMessageReceived(MessageReceivedEvent e) {
 
         String[] message = e.getMessage().getContent().split(" ", 2);
-        for(int i = 0; i < message.length; i++) {
-            System.out.println(message[i]);
-        }
+
 
     }
 
