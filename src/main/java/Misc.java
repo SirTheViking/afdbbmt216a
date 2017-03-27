bcad2f5b866e60400004e14b9d06101238001866
SirTheViking <alexhexan22@gmail.com>
2017-03-27 18:46:09 +0200

    Moved things into variables and changed some comments

--- src/main/java/Misc.java
+++ src/main/java/Misc.java
@@ -1,5 +1,7 @@
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 
@@ -20,23 +22,27 @@ public class Misc extends Command {
             keep this pattern up throughout the document.
             Written on 20/03/2017
          */
+        User author = e.getAuthor();
         MessageChannel channel = e.getChannel();
+        JDA jda = e.getJDA();
+
         String message = e.getMessage().getContent();
+
         String servername = "PRIVATE"; //This is temporary
         if(e.isFromType(ChannelType.TEXT)) {
             servername = e.getGuild().getName();
         }
         /*
             This is for unflipping the table every time
-            somebody, lyric, flips a table, in the
+            somebody, flips a table, in the
             chat. Written on 20/03/2017
          */
-        if(message.startsWith("(") || message.contains("┻")) {
-            channel.sendMessage("┬─┬ノ( ゜-゜ノ) " + e.getAuthor().getAsMention() + " put that shit down").queue();
+        if(message.startsWith("(╯") || message.contains("┻")) {
+            channel.sendMessage("┬─┬ノ( ゜-゜ノ) " + author.getAsMention() + " put that shit down").queue();
         }
 
         if(setAliases(Main.prefixes.get(servername), "ping").contains(message)) {
-            channel.sendMessage("Pong! `" + e.getJDA().getPing() + "ms`").queue();
+            channel.sendMessage("Pong! `" + jda.getPing() + "ms`").queue();
         }
     }
 }
